a750573ae8683caef94bcd6f2f52d03e3c833e97
Lionel-RAKOTOMALALA <rakotomalalalionel32@gmail.com>
2026-04-09 04:54:12 +0300

    feat: add drizzle ORM configuration and authentication schema
    - Created drizzle.config.ts for database configuration.
    - Added SQL migration script for user authentication tables (account, session, user, verification).
    - Generated metadata snapshot and journal for database schema.
    - Implemented authentication logic using better-auth with drizzle adapter.
    - Defined database schema in schema.ts for user, session, account, and verification tables.
    - Established database connection module using NestJS and drizzle ORM.
    - Created UsersController with a session endpoint.
    - Set up UsersModule to manage user-related routes.
    - Added PostCSS configuration file for frontend styling.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { AuthGuard, AuthModule } from '@mguay/nestjs-better-auth';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import {betterAuth} from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { DATABASE_CONNECTION } from './database/database-connection';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    AuthModule.forRootAsync({
+      imports: [DatabaseModule],
+      useFactory: ((database: NodePgDatabase) => ({
+        auth: betterAuth({
+          database: drizzleAdapter(database, {
+            provider: 'pg',
+          }),
+        }),
+      })) as any,
+      inject: [DATABASE_CONNECTION],
+    }),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ],
 })
 export class AppModule {}
